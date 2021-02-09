2baf870fd3077b7dda101e64713aa1f7f98ed1b5
user <user@users-MacBook-Pro.local>
2021-02-09 13:16:20 +0200

    part 2 of code

--- puzzle.py
+++ puzzle.py
@@ -13,3 +13,30 @@ def unique_columns(board):
         for i in range(len(row)):
             columns[i] += row[i]
     return unique_rows(columns)
+
+
+def color_group(board):
+    colors = []
+    for row in range(4, len(board)):
+        clr = ''
+        dif = row - 4
+        start = 4 - dif
+        for ch in range(start, start+5):
+            clr += board[row][ch]
+        colors.append(clr)
+    for ch in range(4, -1, -1):
+        clr = ''
+        start = 4 - ch
+        for row in range(start, start+4):
+            clr += board[row][ch]
+        colors[start] += clr
+    return colors
+
+
+def unique_colors(board):
+    return unique_rows(color_group(board))
+
+
+def validate_board(board):
+    return unique_rows(board) and unique_colors(board) \
+           and unique_columns(board)
